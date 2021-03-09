ef940db059a36a57a6948684b0739908b2f40709
FelixLeung <cpleung1727@gmail.com>
2021-03-09 12:28:24 +0800

    finished setting of html-webpack-plugin

--- src/server/index.js
+++ src/server/index.js
@@ -4,19 +4,27 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
-app.use(express.static('src/client'))
+//2. [Use Client's material]    
+//>> location of asset (e.g. css) 
+//integrated by html-webpack-plugin into 'dist' 
+app.use(express.static('dist'))
 
 console.log(__dirname)
 
+//1. [Use Client's material]    
+//>> what html will user see when they go to your site "/" without following string ?
+//integrated by html-webpack-plugin into 'dist/index.html'
 app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
+    res.sendFile('dist/index.html')
 })
 
-// designates what port the app will listen to for incoming requests
+//3. [Server's material]
+//>>  designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
+//
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
